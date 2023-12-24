2ee712b70a6b38c0307b3b21f57870b83461e4c2
EasySouls <tarjanyicsanad@gmail.com>
2023-12-24 20:45:09 +0100

    Header and footer skeleton

---
+++ prisma/migrations/20231224192228_user_and_todo/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" INTEGER,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/components/Header.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<header>
+	<div>
+		<h1>Amber</h1>
+	</div>
+	<nav>
+		<button>
+			<a href="/">Főoldal</a>
+		</button>
+		<button>
+			<a href="/users">Felhasználók</a>
+		</button>
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+	}
+
+	nav {
+		display: flex;
+	}
+
+	nav > button {
+		border: solid 1px black;
+		background-color: blue;
+	}
+</style>
